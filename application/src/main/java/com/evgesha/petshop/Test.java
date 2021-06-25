36a5c637fb7819f82824b761449315f98635ab1d
Mark Klimenko <22773@bk.ru>
2021-06-25 23:47:44 +0300

    Change Test file

--- application/src/main/java/com/evgesha/petshop/Test.java
+++ application/src/main/java/com/evgesha/petshop/Test.java
@@ -2,6 +2,6 @@ package com.evgesha.petshop;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("here");
+        System.out.println("here here");
     }
 }
